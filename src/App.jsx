e4143e0e982d78b227ac5396c202b5ad586f0475
LilyMorpho <ikaros9903@daum.net>
2025-08-28 10:49:06 +0900

    build with vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter } from "react-router-dom"
+
+import "../public/css/fonts.css"
+import "../public/css/base.css"
+import "../public/css/shop.css"
+
+import Containers from "./Containers"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Containers />
+    </BrowserRouter>
+  )
+}
+
+export default App

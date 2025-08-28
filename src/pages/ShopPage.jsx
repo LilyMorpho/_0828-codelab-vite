02989b0e1d4a8438bf482a0a6b152a377dbece3e
LilyMorpho <ikaros9903@daum.net>
2025-08-28 10:57:47 +0900

    edit typo error

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,42 +1,51 @@
-import { useState } from "react"
-import axios from "axios"
+import { useState } from "react";
+import axios from "axios";
 
-import FormWrapper from "../components/shop/FormWrapper"
-import PrdWrap from "../components/shop/PrdWrap"
+import FormWrapper from "../components/shop/FormWrapper";
+import PrdWrap from "../components/shop/PrdWrap";
 
 export default function ShopPage() {
-  const [prdList, setPrdList] = useState([])
-  const [searchList, setSearchList] = useState([])
+  const [prdList, setPrdList] = useState([]);
+  const [searchList, setSearchList] = useState([]);
   const onGetPrd = async () => {
-    const { data } = await axios.get("/mock/prd.json")
-    setPrdList(data?.list || [])
-    setSearchList(data?.list || [])
-  }
+    const { data } = await axios.get("/mock/prduct.json");
+    setPrdList(data?.list || []);
+    setSearchList(data?.list || []);
+  };
   const onResetPrd = () => {
-    setPrdList([])
-    setSearchList([])
-  }
+    setPrdList([]);
+    setSearchList([]);
+  };
   const onChangeSearch = (search) => {
     if (search === "") {
-      setSearchList(prdList)
+      setSearchList(prdList);
     } else {
       const searchedList = prdList.filter((prd) => {
         return (
           prd.title.toLowerCase().includes(search.toLowerCase()) ||
           prd.description.toLowerCase().includes(search.toLowerCase())
-        )
-      })
-      setSearchList(searchedList)
+        );
+      });
+      setSearchList(searchedList);
     }
-  }
+  };
   return (
     <div className="containers">
-      <FormWrapper onGetPrd={onGetPrd} onResetPrd={onResetPrd} onChangeSearch={onChangeSearch} />
+      <FormWrapper
+        onGetPrd={onGetPrd}
+        onResetPrd={onResetPrd}
+        onChangeSearch={onChangeSearch}
+      />
       <div className="prd-wrapper">
         {searchList.map((prd, idx) => (
-          <PrdWrap key={idx} img={prd.img} title={prd.title} description={prd.description} />
+          <PrdWrap
+            key={idx}
+            img={prd.img}
+            title={prd.title}
+            description={prd.description}
+          />
         ))}
       </div>
     </div>
-  )
+  );
 }

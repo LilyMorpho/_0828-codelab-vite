d275ab61aad035b9f13398aec75266e8842679db
LilyMorpho <ikaros9903@daum.net>
2025-08-29 11:53:59 +0900

    solve error

--- src/components/board/BoardList.jsx
+++ src/components/board/BoardList.jsx
@@ -35,7 +35,7 @@ export default function BoardList() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.map((row) => (
+          {(data || []).map((row) => (
             <TableRow
               key={row.id}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
